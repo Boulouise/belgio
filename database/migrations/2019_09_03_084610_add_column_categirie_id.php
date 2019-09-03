64700a478f38afc5302feebad430b7e963936b79
BOULOUISE <maryamboulouise@gmail.com>
2019-09-03 10:57:11 +0200

    L'ajout des tables service et categorie

---
+++ database/migrations/2019_09_03_084610_add_column_categirie_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnCategirieId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('voitures', function (Blueprint $table) {
+            $table->integer('categorie_id')->unsigned();
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('voitures', function (Blueprint $table) {
+            $table->dropForeign(['categorie_id']);
+            $table->dropColumn('categorie_id');
+        });
+    }
+}
