c646fc16023ee00ac8cdcad79c38281a8eb249a2
BOULOUISE <maryamboulouise@gmail.com>
2019-09-03 11:30:17 +0200

    L'ajout des methodes dans le controllercategorie

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+    public function create(){
+        return view('CreateCategorie');
+    }
+
+    public function index(){
+        $listCategorie = Categorie::paginate(2);
+        $listCategorie->withPath('categorie');
+        return view('categorie' , ['categories'=>$listCategorie]);
+    }
+
+    public function editCategorie($id){
+        $categorie=Categorie::find($id);
+        return view('editCategorie',['categorie'=>$categorie]);
+    }
+    public function deleteCategorie($id){
+        $categorie=Categorie::find($id);
+        $categorie->delete();
+        $listCategorie = Categorie::all();
+        return redirect('/admin');
+      
+    }
 }
